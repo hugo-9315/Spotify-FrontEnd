2f492f1ef0657134a280cb3a8117c4109cb78864
Hugo <hugo.marchao@ynov.com>
2023-12-19 16:56:03 +0100

    initial commit

---
+++ app/(site)/page.tsx
@@ -0,0 +1,34 @@
+import Header from "../components/header";
+import ListItem from "../components/listItem";
+
+export const revalidate = 0;
+const songs = () => {
+  name : "1", "2", "3";
+}
+
+export default function Home() {
+  return (
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        <div className="mb-2">
+          <h1 className="text-white text-3xl font-semibold">Welcome Back !</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:gird-cols-4 gap-3 mt-4">
+            <ListItem image="/images/liked.png" name="Liked Songs" href="liked" />
+            <ListItem image="/images/onrepeat.jpeg" name="OnRepeat" href="onrepeat" />
+            <ListItem image="/images/dailymix.webp" name="Daily Mix" href="dailymix" />
+          </div>
+          </div> 
+      </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-whie text-2xl font-semibold">
+            Newest Songs
+          </h1>
+        </div>
+        <div>
+          List of Songs
+        </div>
+      </div>
+      </div>
+  )
+}
