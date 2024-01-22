42a5c32ec2bd567a1263133c0340d84430a27d31
Hugo <hugo.marchao@ynov.com>
2024-01-22 01:04:00 +0100

    test4

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 import './globals.css'
 import Sidebar from './components/sidebar'
-import Player from './components/player'
+// import Player from './components/player'
 
 const font = Figtree({ subsets: ['latin'] })
 
